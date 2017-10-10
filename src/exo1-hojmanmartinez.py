7fbde44b6ecbead21119b0473e5cbbcf86a709a4
Hermes Martinez <Hermes@eduroam-prg-og-1-31-170.net.univ-paris-diderot.fr>
2017-10-10 15:57:06 +0200

    avance

--- src/exo1-hojmanmartinez.py
+++ src/exo1-hojmanmartinez.py
@@ -46,4 +46,46 @@ def split(filename, randomize=False, proportions=(0.8,0.1,0.1)):
     devfile.close()
     testfile.close()
 
-split(test, True)
+def read_corpus(filename):
+    corpus = []
+    sentence = [['B1','B'],['B2','B']]
+    sentences = []
+    instream = open(filename, 'r')
+    line = instream.readline()
+    while line:
+        word_annotation = line.split()
+        if line == "\n":
+            if len(sentence) != 0:
+                sentence.extend([['F1'],['F2']])
+                sentences.append(sentence)
+            sentence = [['B1','B'],['B2','B']]
+        else:
+            sentence.append([word_annotation[1],word_annotation[3]])
+        line = instream.readline()
+    if len(sentence) != 0:
+        sentence.extend([['F1'],['F2']])
+        sentences.append(sentence)
+    instream.close()
+    #print(sentences[0])
+    for sent in sentences:
+        for i in range(2,len(sent)-2):
+            feats = {}
+            feats["w-2={}".format(sent[i-2][0])] = 1
+            feats["w-1={}".format(sent[i-1][0])] = 1
+            feats["w={}".format(sent[i][0])] = 1
+            feats["w+1={}".format(sent[i+1][0])] = 1
+            feats["w+2={}".format(sent[i+2][0])] = 1
+            feats["t-2={}".format(sent[i-2][1])] = 1
+            feats["t-1={}".format(sent[i-1][1])] = 1
+            corpus.append((feats,sent[i][1]))
+    #print(corpus[0])
+    return corpus
+
+
+
+
+
+
+
+#split(test, True)
+read_corpus(test+".test")
